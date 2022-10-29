05715b82ba123089ff516dcd51ff8c0f96b5b877
suyunovshohjahon08@mail.ru <suyunovshohjahon08@gmail.com>
2022-10-29 20:51:08 +0500

    identify toqson

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-do {
-  var son = +prompt("Enter the positive number!");
+// juft yoki toqligini topish
+let num = parseInt(prompt("Enter the number!"));
 
-  let answer = 1;
-  for (let i = 1; i <= son; i++) {
-    answer *= i;
+if (isNaN(num) || "") {
+  do {
+    alert("Son emasku bu");
+    num = parseInt(prompt("Enter the number!"));
+  } while (true);
+} else {
+  if (num % 2) {
+    alert("Kiritgan soningiz toq son");
+  } else {
+    alert("Kiritgan soningiz juft ekan");
   }
-  console.log(answer + " is the factorial of " + son);
-} while (isNaN(son));
+}
