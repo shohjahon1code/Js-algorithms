3f2afe7ef02b90f3c6b8ce368ceca3b6f92612d1
suyunovshohjahon08@mail.ru <suyunovshohjahon08@gmail.com>
2022-10-31 17:06:01 +0500

    shark

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-let num = 8;
-let line = "";
+let humanSpeed = 5;
+let sharkSpeed = 10;
 
-for (let i = 0; i < num; i++) {
-  for (let j = 0; j < num; j++) {
-    if ((j % 2 === 0 && i % 2 === 0) || (j % 2 === 1 && i % 2 === 1)) {
-      line += " + ";
-    } else {
-      line += " # ";
-    }
-  }
-  console.log(line);
-  line = "";
-}
+let humanDistance = +prompt("humanDistance");
+let sharkDistance = +prompt("sharkDistance");
 
-// Shaxmat doskasi
+humanDistance / humanSpeed < sharkDistance / sharkSpeed
+  ? alert("Akula yetolmaydi")
+  : alert("Akula yetib oladi");
+
+// Akula algorithm
