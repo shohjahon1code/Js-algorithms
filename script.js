99ae81db04875bbe9168844d06d5433b87fe19e2
suyunovshohjahon08@mail.ru <suyunovshohjahon08@gmail.com>
2022-11-03 16:54:01 +0500

    array-algo

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-const productList = {
-  calculateAllPrice: () => {
-    let allPrice = 0;
-    for (const key in productList) {
-      const product = productList[key];
-      if (typeof product === "object") {
-        allPrice += product.price * (product.weight || product.amount);
-      }
-    }
-    return allPrice;
-  },
-  potota: {
-    name: "kartoshka",
-    weight: 4,
-    price: 5000,
-  },
-  tomato: {
-    name: "Pamidor",
-    weight: 3,
-    price: 15000,
-  },
-  cucumber: {
-    name: "Bodring",
-    weight: 2,
-    price: 9000,
-  },
-  meat: {
-    name: "Quy goshti",
-    weight: 1,
-    price: 90000,
-  },
-  carrot: {
-    name: "sabzi",
-    weight: 4,
-    price: 5000,
-  },
-  bread: {
-    name: "non",
-    amount: 4,
-    price: 5000,
-  },
-  cola: {
-    name: "Kola",
-    amount: 4,
-    price: 12000,
-  },
-};
+const arr = [1, 3, 5, 7, 6, 75];
+
+// let answer = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   const array = arr[i];
+//   answer += array;
+// }
+// console.log(answer);
+
+let answer = 0;
+arr.forEach((arr) => {
+  if (arr % 2 === 1) {
+    answer++;
+  }
+});
+console.log(answer);
