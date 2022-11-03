f86377f647011f915b929b6fe2274be290aacc8b
suyunovshohjahon08@mail.ru <suyunovshohjahon08@gmail.com>
2022-11-03 18:50:34 +0500

    stringy

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
-const arr = [1, 3, 5, 7, 6, 75];
-
-// let answer = 0;
-// for (let i = 0; i < arr.length; i++) {
-//   const array = arr[i];
-//   answer += array;
-// }
-// console.log(answer);
-
-let answer = 0;
-arr.forEach((arr) => {
-  if (arr % 2 === 1) {
-    answer++;
+function stringy(size) {
+  let answer = "";
+  for (let i = 0; i < size; i++) {
+    answer += i % 2 ? 0 : 1;
   }
-});
-console.log(answer);
+  return answer;
+}
